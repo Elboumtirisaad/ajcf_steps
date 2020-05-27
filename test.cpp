a3388bc4aa5a88b981c468de3e3f4752996efbcf
Saad EL Boumtiri <elboumtirisaad1993@gmail.com>
2020-05-27 10:31:18 +0200

    debug test

--- test.cpp
+++ test.cpp
@@ -8,6 +8,14 @@ int main()
     std::cout << "Hello\n";
     std::cout << add(2, 3) << "\n";
     std::cout << "double de 5 = " << calcul_double(5) << "\n";
+    int i = 789;
+    std::cout << "i = " << i << std::endl;
+    while (true)
+    {
+        /* code */
+        std::cout << "bon\n";
+    }
+    
     print_current_date();
     print_current_date();
     return 0;
